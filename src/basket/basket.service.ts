68bab4f0187e153109da13a6e7d6b0d1b7b50d6d
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-10-28 20:09:16 +0200

    Add shop and basket services

--- src/basket/basket.service.ts
+++ src/basket/basket.service.ts
@@ -1,17 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { AddProductDto } from './dto/add-product.dto';
-import { AddProductToBasketResponse } from '../interfaces/basket';
+import {
+  AddProductToBasketResponse,
+  GetTotalPriceResponse,
+  ListProductsInBasketResponse,
+  RemoveProductFromBasketResponse,
+} from '../interfaces/basket';
+import { ShopService } from '../shop/shop.service';
 
 @Injectable()
 export class BasketService {
   private items: AddProductDto[] = [];
 
+  constructor(@Inject(ShopService) private shopService: ShopService) {}
+
   add(item: AddProductDto): AddProductToBasketResponse {
+    const { name, count } = item;
+
     if (
-      typeof item.name !== 'string' ||
-      typeof item.count !== 'number' ||
-      item.name === '' ||
-      item.count < 1
+      typeof name !== 'string' ||
+      typeof count !== 'number' ||
+      name === '' ||
+      count < 1 ||
+      !this.shopService.hasProduct(name)
     ) {
       return {
         isSuccess: false,
@@ -24,5 +35,43 @@ export class BasketService {
       isSuccess: true,
       index: this.items.length - 1,
     };
+  }
+
+  remove(index: number): RemoveProductFromBasketResponse {
+    const { length, splice } = this.items;
+    if (index < 0 || index >= length) {
+      return {
+        isSuccess: false,
+      };
+    }
+
+    splice(index, 1);
+
+    return {
+      isSuccess: true,
+    };
+  }
+
+  list(): ListProductsInBasketResponse {
+    return this.items;
+  }
+
+  getTotalPrice(): GetTotalPriceResponse {
+    if (this.items.every((item) => this.shopService.hasProduct(item.name))) {
+      const alternativeBasket = this.items.filter((item) =>
+        this.shopService.hasProduct(item.name),
+      );
+
+      return {
+        isSuccess: false,
+        alternativeBasket,
+      };
+    }
+    return this.items
+      .map(
+        (item) => this.shopService.getPriceOfProduct(item.name) * item.count,
+        23,
+      )
+      .reduce((prev, curr) => prev + curr, 0);
   }
 }
